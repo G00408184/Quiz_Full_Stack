fa46b7bd6854163a771fbcd72319b5c7864e7ce7
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-12-09 20:57:32 +0000

    Adding Nextjs JS

--- next/components/layout/MainNavigation.js
+++ next/components/layout/MainNavigation.js
@@ -1,15 +1,11 @@
 import classes from './MainNavigation.module.css';
 import Link from 'next/link';
-import { useState, useContext } from 'react';
+import {useContext } from 'react';
 import GlobalContext from '../../pages/store/globalContext';
 
 function MainNavigation() {
-    const [popupToggle, setPopupToggle] = useState(false);
     const globalCtx = useContext(GlobalContext);
 
-    function toggleMenuHide() {
-        setPopupToggle((prevState) => !prevState);
-    }
 
     return (
         <header className={classes.header}>
